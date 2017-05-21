8cbb4af3481aa65c23dc8e8e26233211cd6fc9e7
Corvo <doomleika@gmail.com>
2017-05-21 12:16:55 +0800

    debugging testk not working

--- test/phpTest/Bootstrap.php
+++ test/phpTest/Bootstrap.php
@@ -3,7 +3,7 @@ define('PHP_SELF', 'pixmicat.php');
 define('DEBUG', TRUE);
 $_SERVER['HTTP_HOST'] = '127.0.0.1';
 require dirname(__FILE__).'/../../config.php';
-require ROOTPATH.'lib/PMCLibrary.php';
+require ROOTPATH.'lib/pmclibrary.php';
 //require ROOTPATH.'vendor/autoload.php';
-require ROOTPATH.'lib/lib_compatible.php'; // 引入相容函式庫
-require ROOTPATH.'lib/lib_common.php'; // 引入共通函式檔案
+require ROOTPATH.'lib/lib_compatible.php'; // 嚙豬入嚙諛容嚙賜式嚙緩
+require ROOTPATH.'lib/lib_common.php'; // 嚙豬入嚙瑾嚙緬嚙賜式嚙褕殷蕭
